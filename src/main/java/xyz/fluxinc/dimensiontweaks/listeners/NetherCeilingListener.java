4554f14c66009da4c1d48faf20d85a6f3e14cde6
Celeste Magisteel <kamg1134@gmail.com>
2019-09-14 12:17:08 +0100

    Updated to 1.1. Decreased ceiling detection by 2 and prevented pistons moving blocks near ceiling. Also fixed lang issues

--- src/main/java/xyz/fluxinc/dimensiontweaks/listeners/NetherCeilingListener.java
+++ src/main/java/xyz/fluxinc/dimensiontweaks/listeners/NetherCeilingListener.java
@@ -5,6 +5,7 @@ import org.bukkit.block.Biome;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPistonEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
@@ -16,6 +17,8 @@ public class NetherCeilingListener implements Listener {
 
     private DimensionTweaks instance;
     private boolean creativeBypass;
+    private static final int breakLimit = 124;
+    private static final int moveLimit = 125;
     public NetherCeilingListener(DimensionTweaks instance, boolean creativeBypass) { this.instance = instance; this.creativeBypass = creativeBypass; }
 
     @EventHandler
@@ -25,7 +28,7 @@ public class NetherCeilingListener implements Listener {
         // Handle Preventing Accessing the Nether Ceiling
         Biome loc = event.getPlayer().getWorld().getBiome((int)event.getTo().getX(), (int)event.getTo().getZ());
         if (loc != Biome.NETHER) { return; }
-        if (event.getTo().getY() < 127) { return; }
+        if (event.getTo().getY() < moveLimit) { return; }
         event.setCancelled(true);
         HashMap<String, String> var = new HashMap<>();
         var.put("player", event.getPlayer().getName());
@@ -40,7 +43,7 @@ public class NetherCeilingListener implements Listener {
         if (event.getPlayer().getGameMode() == GameMode.CREATIVE && creativeBypass) { return; }
         // Handle Preventing Accessing the Nether Ceiling
         if (event.getBlock().getBiome() != Biome.NETHER) { return; }
-        if (event.getBlock().getY() < 126) { return; }
+        if (event.getBlock().getY() < breakLimit) { return; }
         event.setCancelled(true);
         HashMap<String, String> var = new HashMap<>();
         var.put("player", event.getPlayer().getName());
@@ -56,7 +59,7 @@ public class NetherCeilingListener implements Listener {
         // Handle Preventing Accessing the Nether Ceiling
         Biome loc = event.getTo().getWorld().getBiome(event.getTo().getBlock().getX(), event.getTo().getBlock().getZ());
         if (loc != Biome.NETHER) { return; }
-        if (event.getTo().getBlock().getY() < 126) { return; }
+        if (event.getTo().getBlock().getY() < moveLimit) { return; }
         event.setCancelled(true);
         HashMap<String, String> var = new HashMap<>();
         var.put("player", event.getPlayer().getName());
@@ -73,12 +76,19 @@ public class NetherCeilingListener implements Listener {
         // Handle Preventing Accessing the Nether Ceiling
         Biome loc = event.getTo().getWorld().getBiome(event.getTo().getBlock().getX(), event.getTo().getBlock().getZ());
         if (loc != Biome.NETHER) { return; }
-        if (event.getTo().getBlock().getY() < 126) { return; }
+        if (event.getTo().getBlock().getY() < moveLimit) { return; }
         event.setCancelled(true);
         HashMap<String, String> var = new HashMap<>();
         var.put("player", event.getPlayer().getName());
         var.put("display", event.getPlayer().getDisplayName());
         var.put("permission", "dimensiontweaks.netherceiling.bypass");
         event.getPlayer().sendMessage(instance.generateMessage("netherAccessDenied", var));
+    }
+
+    @EventHandler
+    public void netherCeilingPiston(BlockPistonEvent event) {
+        if (event.getBlock().getY() < breakLimit - 2) { return; }
+        if (event.getBlock().getBiome() != Biome.NETHER) { return; }
+        event.setCancelled(true);
     }
 }
